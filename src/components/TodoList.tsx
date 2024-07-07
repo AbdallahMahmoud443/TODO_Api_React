79cac2ef696a1b65626e4369aae82bbb58f425dc
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-07-07 18:02:18 +0300

    adding laoding and install reactquery

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,6 +10,10 @@ const TodoList = () => {
 
   // status for store fetched todos
   const [todos, setTodos] = useState([]);
+  const [isLoading,setIsLoading] = useState<boolean>(true);
+
+  
+
 
   // Fetch Data From API Using UseEffect
   useEffect(() => {
@@ -21,13 +25,17 @@ const TodoList = () => {
       }).then((data) => setTodos(data?.data?.todos)).catch((err) => console.log('THE ERROR:', err))
     } catch (error) {
       console.log(error);
+    }finally{
+      setIsLoading(false);
     }
   }, [userData?.jwt])
 
-  return (
+  // if data loading 
+  if(isLoading) return <p className="w-full font-semibold">Data is Loading.....⏳</p>
 
+  return (
     <div className="space-y-1 ">
-      {todos.map((todo) => (
+      {todos.length ? todos.map((todo) => (
           <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md
             w-full even:bg-gray-100">
             <p className="w-full font-semibold">
@@ -37,8 +45,8 @@ const TodoList = () => {
               <Button variant={"default"} size={"sm"}>Edit </Button>
               <Button variant={"danger"} size={"sm"}>Remove</Button>
             </div>
-        </div>))}
-
+        </div>)) :<p className="w-full font-semibold">Not found Todos yet....😌</p>
+        }
     </div>
   )
 };
